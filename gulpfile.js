51d38e1a44fce1f1c2d06daa37d93d8204e864a1
Anastasiia <karn-av@protonmail.com>
2020-02-14 22:35:43 +0300

    Исправляет импорт scss

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,39 @@ var plumber = require('gulp-plumber');
 var sourcemap = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
+//var csso = require('gulp-csso');
 var autoprefixer = require('autoprefixer');
 var server = require('browser-sync').create();
+var sassGlob = require('gulp-sass-glob');
+
+//var del = require('del');
+
+//gulp.task("clean", function () {
+//  return del("build");
+//});
+
+//gulp.task("copy", function () {
+//  return gulp.src([
+//    "source/fonts/**/*.{woff,woff2}",
+//    "source/img/**",
+//    "source/js/**",
+//    "source/*.html"
+//  ], {
+//    base: "source"
+//  })
+//  .pipe(gulp.dest("build"));
+//});
 
 gulp.task("css", function() {
   return gulp.src("source/sass/style.scss")
+    .pipe(sassGlob())
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
 
 gulp.task("server", function () {
@@ -29,9 +49,8 @@ gulp.task("server", function () {
     ui: false
   });
   
-  gulp.watch("source/sass/**/*.{sass,scss}", gulp.series("css"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
-
 });
 
 gulp.task("start", gulp.series("css", "server"));
